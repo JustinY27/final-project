66c8640c3cd4c46f0d9bd0ea5a3e4f52bf68d917
JustinY27 <justinry27@email.com>
2023-02-09 20:58:24 -0500

    final commit

--- data-download.R
+++ data-download.R
@@ -1,5 +1,6 @@
 library(tidycensus)
 library(tidyverse)
+library(tigris)
 
 dallas_bachelors <- 
   get_acs(geography = "tract",
@@ -11,3 +12,14 @@ dallas_bachelors <-
 
 write_rds(dallas_bachelors, "data/dallas.rds")
 
+us_median_age <- get_acs(
+  geography = "state",
+  variables = "B01002_001",
+  year = 2019,
+  survey = "acs1",
+  geometry = TRUE,
+  resolution = "20m"
+) %>%
+  shift_geometry()
+
+write_rds(us_median_age, "data/median_age.rds")
